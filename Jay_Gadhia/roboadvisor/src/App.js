f3fac4fa1b4b50f0f84afa718800916a06063722
JayGadhia1 <113377153+JayGadhia1@users.noreply.github.com>
2024-03-11 22:50:48 -0500

    Updated the bug on the MCQ part

--- Jay_Gadhia/roboadvisor/src/App.js
+++ Jay_Gadhia/roboadvisor/src/App.js
@@ -31,67 +31,68 @@ function App() {
           <label>
             {/*Question 1}{*/}
             <p>Question 1<br></br>In general, how would your best friend describe you as a risk taker?</p>
-            <input type="radio" value="1" checked={selectedOption === '1'} onChange={handleOptionChange} /> Real risk avoider
-            <br></br><input type="radio" value="2" checked={selectedOption === '2'} onChange={handleOptionChange} /> Cautious
-            <br></br><input type="radio" value="3" checked={selectedOption === '3'} onChange={handleOptionChange} /> Willing to take risks after completing adequate research
-            <br></br><input type="radio" value="4" checked={selectedOption === '4'} onChange={handleOptionChange} /> Real gambler
+                     <input type="radio" name="question1" value="1" /> Real risk avoider
+            <br></br><input type="radio" name="question1" value="2" /> Cautious
+            <br></br><input type="radio" name="question1" value="3" /> Willing to take risks after completing adequate research
+            <br></br><input type="radio" name="question1" value="4" /> Real gambler
           </label>
 
           {/*Question 2}{*/}
           <label>
             <p>Question 2<br></br>You are on a TV game show and can choose one of the following. Which would you take?</p>
-            <input type="radio" value="5" checked={selectedOption === '5'} onChange={handleOptionChange} /> $1,000 in cash
-            <br></br><input type="radio" value="6" checked={selectedOption === '6'} onChange={handleOptionChange} /> 50% chance at winning $5,000
-            <br></br><input type="radio" value="7" checked={selectedOption === '7'} onChange={handleOptionChange} /> 25% chance at winning $10,000
-            <br></br><input type="radio" value="8" checked={selectedOption === '8'} onChange={handleOptionChange} /> 5% chance at winning $100,000
+                     <input type="radio" name="question2" value="1"  /> $1,000 in cash
+            <br></br><input type="radio" name="question2" value="2"  /> 50% chance at winning $5,000
+            <br></br><input type="radio" name="question2" value="3"  /> 25% chance at winning $10,000
+            <br></br><input type="radio" name="question2" value="4"  /> 5% chance at winning $100,000
           </label>
           
           {/*Question 3}{*/}
           <label>
             <p>Question 3<br></br>In general, how would your best friend describe you as a risk taker?</p>
-            <input type="radio" value="1" checked={selectedOption === '1'} onChange={handleOptionChange} /> A real risk avoider
-            <br></br><input type="radio" value="2" checked={selectedOption === '2'} onChange={handleOptionChange} /> Cautious
-            <br></br><input type="radio" value="3" checked={selectedOption === '3'} onChange={handleOptionChange} /> Willing to take risks after completing adequate research
-            <br></br><input type="radio" value="4" checked={selectedOption === '4'} onChange={handleOptionChange} /> A real gambler
+                     <input type="radio" name="question3" value="1" /> A real risk avoider
+            <br></br><input type="radio" name="question3" value="2" /> Cautious
+            <br></br><input type="radio" name="question3" value="3" /> Willing to take risks after completing adequate research
+            <br></br><input type="radio" name="question3" value="4" /> A real gambler
           </label>
 
           {/*Question 4}{*/}
           <label>
             <p>Question 4<br></br>In general, how would your best friend describe you as a risk taker?</p>
-            <input type="radio" value="1" checked={selectedOption === '1'} onChange={handleOptionChange} /> A real risk avoider
-            <br></br><input type="radio" value="2" checked={selectedOption === '2'} onChange={handleOptionChange} /> Cautious
-            <br></br><input type="radio" value="3" checked={selectedOption === '3'} onChange={handleOptionChange} /> Willing to take risks after completing adequate research
-            <br></br><input type="radio" value="4" checked={selectedOption === '4'} onChange={handleOptionChange} /> A real gambler
+                     <input type="radio" name="question4" value="1"  /> A real risk avoider
+            <br></br><input type="radio" name="question4" value="2" /> Cautious
+            <br></br><input type="radio" name="question4" value="3" /> Willing to take risks after completing adequate research
+            <br></br><input type="radio" name="question4" value="4" /> A real gambler
           </label>
 
           {/*Question 5}{*/}
           <label>
             <p>Question 5<br></br>In general, how would your best friend describe you as a risk taker?</p>
-            <input type="radio" value="1" checked={selectedOption === '1'} onChange={handleOptionChange} /> A real risk avoider
-            <br></br><input type="radio" value="2" checked={selectedOption === '2'} onChange={handleOptionChange} /> Cautious
-            <br></br><input type="radio" value="3" checked={selectedOption === '3'} onChange={handleOptionChange} /> Willing to take risks after completing adequate research
-            <br></br><input type="radio" value="4" checked={selectedOption === '4'} onChange={handleOptionChange} /> A real gambler
+                     <input type="radio" name="question5" value="1"  /> A real risk avoider
+            <br></br><input type="radio" name="question5" value="2" /> Cautious
+            <br></br><input type="radio" name="question5" value="3" /> Willing to take risks after completing adequate research
+            <br></br><input type="radio" name="question5" value="4" /> A real gambler
           </label>
 
           {/*Question 6}{*/}
           <label>
             <p>Question 6<br></br>In general, how would your best friend describe you as a risk taker?</p>
-            <input type="radio" value="1" checked={selectedOption === '1'} onChange={handleOptionChange} /> A real risk avoider
-            <br></br><input type="radio" value="2" checked={selectedOption === '2'} onChange={handleOptionChange} /> Cautious
-            <br></br><input type="radio" value="3" checked={selectedOption === '3'} onChange={handleOptionChange} /> Willing to take risks after completing adequate research
-            <br></br><input type="radio" value="4" checked={selectedOption === '4'} onChange={handleOptionChange} /> A real gambler
+                     <input type="radio" name="question6" value="1"  /> A real risk avoider
+            <br></br><input type="radio" name="question6" value="2" /> Cautious
+            <br></br><input type="radio" name="question6" value="3" /> Willing to take risks after completing adequate research
+            <br></br><input type="radio" name="question6" value="4" /> A real gambler
           </label>
           
           {/*Question 7}{*/}
           <label>
             <p>Question 7<br></br>In general, how would your best friend describe you as a risk taker?</p>
-            <input type="radio" value="1" checked={selectedOption === '1'} onChange={handleOptionChange} /> A real risk avoider
-            <br></br><input type="radio" value="2" checked={selectedOption === '2'} onChange={handleOptionChange} /> Cautious
-            <br></br><input type="radio" value="3" checked={selectedOption === '3'} onChange={handleOptionChange} /> Willing to take risks after completing adequate research
-            <br></br><input type="radio" value="4" checked={selectedOption === '4'} onChange={handleOptionChange} /> A real gambler
+                     <input type="radio" name="question7" value="1"  /> A real risk avoider
+            <br></br><input type="radio" name="question7" value="2" /> Cautious
+            <br></br><input type="radio" name="question7" value="3" /> Willing to take risks after completing adequate research
+            <br></br><input type="radio" name="question7" value="4" /> A real gambler
           </label>
           <button type="submit">Submit</button>
         </fieldset>
+
       </form>  
     </div>
   );
