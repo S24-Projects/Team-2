afc91925064adadba00156c334b183a69bcc1e6a
calvinjxie <calvinjxie@gmail.com>
2024-03-12 22:32:50 -0500

    started js/html for mc question

--- Calvin_Xie/script.js
+++ Calvin_Xie/script.js
@@ -4,3 +4,20 @@ function displayInput() {
     displayArea.innerHTML = "<p>Your input: " + userInput + "</p>";
   }
   
+var submitAnswer = function(){
+  var radios = document.getElementById("choice");
+  var val = "";
+  for(var i = 0, length = radios.length; i<length;++i)
+  {
+    if(radios[i].checked)
+    {
+      val = radios[i].value;
+      document.getElementById("display_area").innerHTML = "Correct";
+      break;
+    }
+  }
+  if(val == "")
+  {
+    document.getElementById.innerHTML = "Please choose an answer";
+  }
+};
